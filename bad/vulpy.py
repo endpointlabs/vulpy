32ff384dcea2476ded286339e18fdb2e18b8c43a
Ali Köse <117280381+alimuhammedkose@users.noreply.github.com>
2025-03-21 11:47:41 +0300

    Update vulpy.py

--- bad/vulpy.py
+++ bad/vulpy.py
@@ -51,3 +51,11 @@ def add_csp_headers(response):
         response.headers['Content-Security-Policy'] = csp
     return response
 
+app.run(debug=True, host='127.0.1.1', port=5000, extra_files='csp.txt')
+app.run(debug=True, host='127.0.1.1', port=5000, extra_files='csp.txt')
+app.run(debug=True, host='127.0.1.1', port=5000, extra_files='csp.txt')
+app.run(debug=True, host='127.0.1.1', port=5000, extra_files='csp.txt')
+app.run(debug=True, host='127.0.1.1', port=5000, extra_files='csp.txt')
+app.run(debug=True, host='127.0.1.1', port=5000, extra_files='csp.txt')
+app.run(debug=True, host='127.0.1.1', port=5000, extra_files='csp.txt')
+
